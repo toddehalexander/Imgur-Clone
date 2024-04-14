d4590e97a0fa4e3f4153cb8524980ac11c016e5d
benninghoven <devinbenninghoven@gmail.com>
2024-04-13 18:23:31 -0700

    added initial backend structure

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,13 @@
-print("backend")
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/api', methods=['POST'])
+def api():
+    data = request.get_json()
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
